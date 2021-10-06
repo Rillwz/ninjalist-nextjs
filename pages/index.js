9ccc1466afd0670fb75a619dac50833c1575b3a0
Rillwz <Yusril.edmodo25@gmail.com>
2021-10-07 00:30:56 +0700

    add other component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Head from "next/head";
-import Image from "next/image";
+import Navbar from "../comps/Navbar";
+import Footer from "../comps/Footer";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
       <p>lorem ipsum</p>
+      <Footer />
     </div>
   );
 }
