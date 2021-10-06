439c6e5611eedabd26d5e2fbe4b5bba45adf08b8
Rillwz <Yusril.edmodo25@gmail.com>
2021-10-07 00:39:00 +0700

    Linking between pages

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Navbar from "../comps/Navbar";
 import Footer from "../comps/Footer";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,6 +10,9 @@ export default function Home() {
       <Navbar />
       <h1>Homepage</h1>
       <p>lorem ipsum</p>
+      <Link href="/ninjas">
+        <a href="/ninjas">See Ninja Listing</a>
+      </Link>
       <Footer />
     </div>
   );
