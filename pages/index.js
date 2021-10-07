fa166e2b86b3bfc0e75844501560f56353fc3171
Rillwz <Yusril.edmodo25@gmail.com>
2021-10-07 11:05:42 +0700

    Metadata next/head

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,20 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>lorem ipsum</p>
-      <Link href="/ninjas">
-        <a className={styles.btn} href="/ninjas">
-          See Ninja Listing
-        </a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas"/>
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>lorem ipsum</p>
+        <Link href="/ninjas">
+          <a className={styles.btn} href="/ninjas">
+            See Ninja Listing
+          </a>
+        </Link>
+      </div>
+    </>
   );
 }
