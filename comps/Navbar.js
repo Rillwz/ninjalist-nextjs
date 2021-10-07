c9ef5a579ff3add1464684222ea2cb0b132ba2ee
Rillwz <Yusril.edmodo25@gmail.com>
2021-10-07 10:59:57 +0700

    Images

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,16 +1,23 @@
-import Link from 'next/link'
+import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
-    return (
-        <nav>
-            <div className="logo">
-                <h1>Ninja List</h1>
-            </div>
-            <Link href="/"><a>Home</a></Link>
-            <Link href="/about"><a>About</a></Link>
-            <Link href="/ninjas"><a>NinjaListing</a></Link>
-        </nav>
-      );
-}
- 
+  return (
+    <nav>
+      <div className="logo">
+        <Image src="/vercel.svg" width={120} height={80} />
+      </div>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <Link href="/ninjas">
+        <a>NinjaListing</a>
+      </Link>
+    </nav>
+  );
+};
+
 export default Navbar;
